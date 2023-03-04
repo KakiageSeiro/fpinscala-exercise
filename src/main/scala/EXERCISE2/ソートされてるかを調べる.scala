1eeb00786ecc18923f93fad5bca7fc43b4a976d3
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2023-03-05 08:34:37 +0900

    Scala3だとIntellijでオブジェクトは参照できるけどメソッドは参照できなかったり、テストをクラス単位でしかできなかったりしたので下げた

--- src/main/scala/EXERCISE2/ソートされてるかを調べる.scala
+++ src/main/scala/EXERCISE2/ソートされてるかを調べる.scala
@@ -5,12 +5,15 @@ import scala.annotation.tailrec
 // EXERCISE 2.2
 object ソートされてるかを調べる {
   // asってなんの略？
-  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean =
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
     @tailrec
-    def go(n: Int): Boolean =
+    def go(n: Int): Boolean = {
       if (n >= as.length - 1) true // 配列の最後まで見たら終わり
       else if (!ordered(as(n), as(n + 1))) false // 2つの要素の並びがorderedにしたがっているか
       else go(n + 1)
 
+    }
+
     go(0)
+  }
 }
