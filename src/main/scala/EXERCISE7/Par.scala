620cb10aafd64e851cde8fbac71dc53b62c056b3
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2023-05-20 09:32:01 +0900

    ExecutorServiceを使う実装に修正

--- src/main/scala/EXERCISE7/Par.scala
+++ src/main/scala/EXERCISE7/Par.scala
@@ -1,27 +1,43 @@
 package EXERCISE7
 
+import java.util.concurrent.{Callable, ExecutorService, Future}
+import scala.concurrent.duration.TimeUnit
+
 case class Par[A](run: A){
 
 }
 
 object Par {
-  // 最初のsum
-//  def sum(ints: IndexedSeq[Int]): Int =
-//    if (ints.size <= 1)
-//      ints.headOption getOrElse 0
-//    else {
-//      val (l, r) = ints.splitAt(ints.length / 2)
-//      sum(l) + sum(r)
-//    }
+  // unitのために必要なエイリアス
+  type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: => A): Par[A] = Par(a)
-  def get[A](a: Par[A]): A = a.run
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
-  // EXERCISE7.1
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    override def isDone: Boolean = true
+    override def get(timeout: Long, units: TimeUnit): A = get
+    override def isCancelled: Boolean = false
+    override def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  // この実装の問題
+  // 1. Callableのcall実行中はブロックする
+  // 2. es.submitとa(es)で2つのスレッドを使う。callでは1つの処理しかしてないので、そのスレッドでやればよい。
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(new Callable[A] {
+      def call: A = a(es).get
+    })
+
+  // fは本スレッドでやる。別スレッドでやりたい時はfork(Par.map2(a, b)(f))のようにforkで包む
   def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
-    unit(f(a.run, b.run))
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
 
-  // mat2を使うsum
   def sum(ints: IndexedSeq[Int]): Par[Int] =
     if (ints.size <= 1)
       Par.unit(ints.headOption getOrElse 0)
