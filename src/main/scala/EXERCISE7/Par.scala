85b1eba2b0eac9950fee64f75193a185031768b9
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2023-05-27 09:45:02 +0900

    タイムアウトを実装したmap2を追加してみたが、うまく動いていない

--- src/main/scala/EXERCISE7/Par.scala
+++ src/main/scala/EXERCISE7/Par.scala
@@ -1,11 +1,8 @@
 package EXERCISE7
 
-import java.util.concurrent.{Callable, ExecutorService, Future}
-import scala.concurrent.duration.TimeUnit
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
-case class Par[A](run: A){
-
-}
+case class Par[A](run: A) {}
 
 object Par {
   // unitのために必要なエイリアス
@@ -15,8 +12,12 @@ object Par {
 
   private case class UnitFuture[A](get: A) extends Future[A] {
     override def isDone: Boolean = true
+
     override def get(timeout: Long, units: TimeUnit): A = get
+
     override def isCancelled: Boolean = false
+
+    // cancel関数を呼ぶのに引数でキャンセルしないというのはどういう用途？
     override def cancel(evenIfRunning: Boolean): Boolean = false
   }
 
@@ -38,14 +39,43 @@ object Par {
       UnitFuture(f(af.get, bf.get))
     }
 
-  // EXERCISE 7.3
-  // 答え見た。並行処理がわからなすぎて、答えを見ても理解できない。ので一旦そっちを学習する。
-
   def sum(ints: IndexedSeq[Int]): Par[Int] =
     if (ints.size <= 1)
-      Par.unit(ints.headOption getOrElse 0)
+      unit(ints.headOption getOrElse 0)
     else {
       val (l, r) = ints.splitAt(ints.length / 2)
-      Par.map2(sum(l), sum(r))(_ + _)
+      map2(sum(l), sum(r))(_ + _)
     }
+
+  def map2Timeouts[A, B, C](parA: ExecutorService => Future[A], parB: ExecutorService => Future[B])(abからcにするf: (A, B) => C): ExecutorService => Future[C] = {
+    es: ExecutorService =>
+      new Future[C] {
+        private val futureA: Future[A] = parA(es)
+        private val futureB: Future[B] = parB(es)
+        // @volatileによって、マシン語レベルでキャッシュされることによりスレッドごとに違う値になってしまう現象を防ぐことができるみたい。理解が浅いので注意。
+        @volatile private var cache: Option[C] = None
+
+        def isDone: Boolean = cache.isDefined
+
+        // 実質タイムアウトなしになるでかい時間指定
+        def get(): C = get(Long.MaxValue, TimeUnit.NANOSECONDS)
+
+        def get(timeout: Long, units: TimeUnit): C = {
+          val timeoutNanos = TimeUnit.NANOSECONDS.convert(timeout, units)
+          val 開始時間 = System.nanoTime
+          val a = futureA.get(timeoutNanos, TimeUnit.NANOSECONDS)
+          val 経過時間 = System.nanoTime - 開始時間
+          val b = futureB.get(timeoutNanos - 経過時間, TimeUnit.NANOSECONDS)
+          val c = abからcにするf(a, b)
+          cache = Some(c)
+          c
+        }
+
+        def isCancelled: Boolean = futureA.isCancelled || futureB.isCancelled
+
+        def cancel(evenIfRunning: Boolean): Boolean =
+          futureA.cancel(evenIfRunning) || futureB.cancel(evenIfRunning)
+      }
+  }
 }
+
--- src/test/scala/EXERCISE7/ParTest.scala
+++ src/test/scala/EXERCISE7/ParTest.scala
@@ -4,16 +4,87 @@ import org.scalatest.concurrent.TimeLimits
 import org.scalatest.diagrams.Diagrams
 import org.scalatest.flatspec.AnyFlatSpec
 
+import java.util.concurrent.{ExecutorService, Executors, Future, TimeUnit}
+import scala.concurrent.TimeoutException
+
 
 class ParTest extends AnyFlatSpec with Diagrams with TimeLimits {
   "map2" should "ペア" in {
-    val result = Par.map2(Par.unit(10), Par.unit(20))(_ + _)
+    val result: Par.Par[Int] = Par.map2(Par.unit(10), Par.unit(20))(_ + _)
     assert(result == Par.unit(30))
   }
 
   "sum" should "たす" in {
     val result = Par.sum(IndexedSeq(1, 2, 3, 4, 5))
     assert(result == Par.unit(15))
+  }
+
+  "map2Timeouts" should "たす" in {
+    // newFixedThreadPoolはスレッド数を指定してExecutorServiceを作成する
+    val es: ExecutorService = Executors.newFixedThreadPool(2)
+
+    // テスト用の関数
+    def sum(a: Int, b: Int): Int = a + b
+
+    // ここがPar.Parになるのやだな。objectではなくcase classに全部定義してしまえばよかったけどめんどくさいからこのままで
+    val parA: Par.Par[Int] = Par.unit {
+      Thread.sleep(1000) // 1秒待機
+      1
+    }
+    val parB: Par.Par[Int] = Par.unit(2)
+    val parC: ExecutorService => Future[Int] = Par.map2Timeouts(parA, parB)(sum)
+    val result = Par.run(es)(parC)
+    assert(result.get() == 3)
+
+    es.shutdown()
+  }
+
+  "map2Timeouts" should "タイムアウトしない" in {
+    val es: ExecutorService = Executors.newFixedThreadPool(2)
+
+    // テスト用の関数
+    def sum(a: Int, b: Int): Int = a + b
+
+    val parA: Par.Par[Int] = Par.unit {
+      Thread.sleep(2000) // 2秒待機
+      1
+    }
+    val parB: Par.Par[Int] = Par.unit {
+      Thread.sleep(1000) // 1秒待機
+      2
+    }
+    val parC = Par.map2Timeouts(parA, parB)(sum)
+    val result = Par.run(es)(parC)
+
+    // タイムアウト時間を指定しないのでタイムアウトにならない
+    assert(result.get() == 3)
+
+    es.shutdown()
+  }
+
+  "map2Timeouts" should "タイムアウトする" in {
+    val es: ExecutorService = Executors.newFixedThreadPool(2)
+
+    // テスト用の関数
+    def sum(a: Int, b: Int): Int = a + b
+
+    val parA: Par.Par[Int] = Par.unit {
+      Thread.sleep(2000) // 2秒待機
+      1
+    }
+    val parB: Par.Par[Int] = Par.unit {
+      Thread.sleep(1000) // 1秒待機
+      2
+    }
+    val parC = Par.map2Timeouts(parA, parB)(sum)
+    val result = Par.run(es)(parC)
+
+    // タイムアウトエラーがスローされるはずだがされない。なんでだ
+    intercept[TimeoutException] {
+      result.get(1, TimeUnit.NANOSECONDS)
+    }
+
+    es.shutdown()
   }
 }
 
