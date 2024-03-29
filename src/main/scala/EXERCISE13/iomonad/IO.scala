4cfdff79dafde1ab3769a1ba090cb30f742eec3c
KakiageSeiro <frederika07_bernkastel@yahoo.co.jp>
2024-03-30 08:06:06 +0900

    一旦モノイドではなくなって、Inputができるようになった

--- src/main/scala/EXERCISE13/iomonad/IO.scala
+++ src/main/scala/EXERCISE13/iomonad/IO.scala
@@ -1,15 +1,25 @@
 package EXERCISE13.iomonad
 
-trait IO { self =>
-  def run(): Unit
+trait IO[A] { self =>
+  def run(): A
 
   // いつもJavaでやってる、クラスのインスタンスを不変にしたいので、変更したい部分だけ受け取って、それ以外はそのままのインスタンスを新たにnewして返すやつ
-  def ++(io: IO): IO = new IO {
-    def run(): Unit = { self.run(); io.run() } // 自分のrunと引数のrunを連続で実行するよ〜
+//  def ++(io: IO): IO = new IO {
+//    def run(): Unit = { self.run(); io.run() } // 自分のrunと引数のrunを連続で実行するよ〜
+//  }
+
+  def map[B](f: A => B): IO[B] = new IO[B] {
+    // トレイトの型パラメータをBでnewしているので、run()もAではなくBを返すようになったよ
+    def run(): B = f(self.run())
+  }
+
+  def flatMap[B](f: A => IO[B]): IO[B] = new IO[B] {
+    // トレイトの型パラメータをBでnewしているので、run()もAではなくBを返すようになったよ
+    def run(): B = f(self.run()).run()
   }
 }
 
 object IO {
   // runに何もないインスタンスを作るやつ。これいらなくね？となるが、これが単位元で++がopのモノイド
-  def empty: IO = new IO { def run(): Unit = () }
+//  def empty: IO = new IO { def run(): Unit = () }
 }
